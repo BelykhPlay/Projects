cb4ba9185fc91f1c6b64efe28835640a5a4bc864
BelykhPlay <eg.belykh@yandex.ru>
2022-12-06 12:41:58 +0300

    completed completely

---
+++ src/Task11_25/Main.java
@@ -0,0 +1,10 @@
+package Task11_25;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new WindowApplication().setVisible(true));
+    }
+}
